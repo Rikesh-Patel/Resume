b326147dab4818ba073039e0c19499589ca6ab6b
Rikesh-Patel <103740388+Rikesh-Patel@users.noreply.github.com>
2022-04-17 17:55:51 -0700

    Initial Commit

---
+++ rbg.js
@@ -0,0 +1,25 @@
+var images = [
+     '../images/1.jpeg',
+     '../images/2.jpeg',
+     '../images/3.jpeg',
+     '../images/4.jpg'];
+
+var img = document.getElementById("img");
+function imgDisp(num) {
+
+    var size = images.length;
+    var num = Math.floor(size * Math.random());
+    img.style.backgroundImage = 'url("' + images[num] + '")';
+    img.style.backgroundRepeat = 'no-repeat';
+
+}
+imgDisp();
+
+/*    var x = Math.floor(size * Math.random());
+    console.log(x);
+    var element = document.getElementsByClassName('home');
+    console.log(element);
+    element[0].style["background"] = "url("+ images[x] + ") no-repeat;";
+    document.addEventListener("home", randomImage);
+
+} */
